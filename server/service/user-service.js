ddab7002940345a86d74a455635023fde5cf4f7d
Ural Guru <gizzatullinu@gmail.com>
2023-10-23 14:34:21 +0500

    SERVICE: conectUserControllersWithUserService

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,4 +1,4 @@
-const UserModel = require('../models/user-model');
+const userModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
@@ -7,14 +7,14 @@ const UserDto = require('../dtos/user-dto');
 
 class UserService {
     async registration(email, password) {
-        const candidate = await UserModel.findOne({ email });
+        const candidate = await userModel.findOne({ email });
         if (candidate) {
             throw new Error(`Пользователь с почтовым адресом ${email} уже существует`)
         };
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
 
-        const user = await UserModel.create({ email, password: hashPassword });
+        const user = await userModel.create({ email, password: hashPassword });
         await mailService.sendActivationMail(email, activationLink);
 
         const userDto = new UserDto(user); //email, id, isActivated
