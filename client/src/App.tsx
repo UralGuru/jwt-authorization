a23dc82feba294c6dfc74fe6ad6a6c23b85149a2
Ural Guru <gizzatullinu@gmail.com>
2023-10-24 17:51:48 +0500

    CLIENT: getUsers

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,72 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import './App.css'
 import Login from './components/login'
 import { Context } from './main'
 import { observer } from 'mobx-react-lite';
+import { User } from './models/interfaces';
+import UserService from './services/users.service';
 
 function App() {
   const { store } = useContext(Context);
+  const [users, setUsers] = useState<User[]>([]);
+
+
   useEffect(() => {
     if (localStorage.getItem('token')) {
       store.checkAuth()
     }
   }, []);
 
+  async function getUsers() {
+    try {
+      const response = await UserService.getUsers();
+      setUsers(response.data)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   if (store.isLoading) {
     return <div>Loading...</div>
   };
 
   if (!store.isAuth) {
-    return <Login />
+    return (
+      <>
+        <Login />
+        <div>
+          <button onClick={getUsers}>Получить список пользователей</button>
+        </div>
+
+        <div>
+          {
+            users.map(user => {
+              return <div key={user.id}>{user.id} {user.email}</div>
+            })
+          }
+        </div>
+      </>
+    )
   };
 
   return (
     <>
       <h1>Пользователь</h1>
-      <h2>{`Профиль ${store.user.email}`}</h2>
+      <h2>{`Профиль ${store.user.email}`} </h2>
+      <h2>{`Аккаунт подтвержден ${store.user.isActivated ? "✅" : "❌"}`} </h2>
       <button onClick={() => store.logout()}>Выйти</button>
+
+      <div>
+        <button onClick={getUsers}>Получить список пользователей</button>
+      </div>
+
+      <div>
+        {
+          users.map(user => {
+            return <div key={user.id}>{user.id} {user.email}</div>
+          })
+        }
+      </div>
     </>
   )
 }
