1a5b049d12a56639c35b982cfd4ee4483c586e8e
Ural Guru <gizzatullinu@gmail.com>
2023-10-24 13:36:43 +0500

    SERVER: createLogout

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -54,7 +54,13 @@ class UserService {
             ...tokens,
             user: userDto
         }
-    }
+    };
+
+    async logout(refreshToken) {
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
+
+    };
 };
 
 module.exports = new UserService();
